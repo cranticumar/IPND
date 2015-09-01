5d8d32036dc8b1131209aea6530ce8b088552dac
cranticumar <cranticumar@gmail.com>
2015-09-01 12:02:27 +0530

    Problem Euler 5  - LCM

---
+++ Stage2/Project_Euler/project_euler_5_lcm.py
@@ -0,0 +1,22 @@
+﻿def gen_lcm(series):
+    '''
+    Generates LCM of series numbers
+    '''
+    quotients = series
+    lcm = 1
+    m = 2
+    while True:
+        reminders = [n % m for n in quotients]
+        print reminders
+        if 0 in reminders:
+            quotients = [n / m if n % m == 0 else n for n in quotients]
+            lcm = lcm * m
+        else:
+            m += 1
+        print quotients
+        if reduce(lambda x, y: x * y, quotients) == 1:
+            break
+
+    return lcm
+
+print gen_lcm(map(int, raw_input("Enter series of number with space in between:").split(" ")))
