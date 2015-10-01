aea6792c250ebe2a9eec9a1d9fbba2dca938a027
cranticumar <cranticumar@gmail.com>
2015-10-01 11:59:18 +0530

    removing unnecessary files

--- Stage4/learningatudacity/sql.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-
-db = sqlite3.connect(':memory:')
-db.execute("CREATE TABLE LINKS (id integer, title text, " +
-           "submitter_id text, submitted_time integer, comments text, votes integer)")
